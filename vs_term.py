aa1cf51f786abf3b5c7885618c45adfa489ef909
Akufoolz <akufoolz@gmail.com>
2018-10-26 10:35:10 -0600

    added make_span_list function

--- vs_term.py
+++ vs_term.py
@@ -1,3 +1,4 @@
+import calendar
 from datetime import date
 from itertools import repeat
 
@@ -15,6 +16,24 @@ def create_date(tup, d):
     return new_date
 
 
+def add_months(in_date, months):
+    months_count = in_date.month + months
+
+    year = in_date.year + int(months_count / 12)
+
+    month = (months_count % 12)
+    if month == 0:
+        month = 12
+
+    day = in_date.day
+    last_day_of_month = calendar.monthrange(year, month)[1]
+    if day > last_day_of_month:
+        day = last_day_of_month
+
+    new_date = date(year, month, day)
+    return new_date
+
+
 def get_terms(count, total, lst):
     if count < int(total):
         new_list = lst + [(get_input('Enter term date\n'))]
@@ -28,20 +47,37 @@ def split_term_date(term):
 
 
 def make_term_list(lst):
-    lst2 = map(split_term_date, lst)
+    lst2 = list(map(split_term_date, lst))
     return lst2
 
 
 def make_date_list(lst):
-    lst2 = map(create_date, lst, repeat(1))
+    lst2 = list(map(create_date, lst, repeat(1)))
     return lst2
 
 
+def make_span_list(count, lst, lst2):
+    if count < len(lst) - 1:
+        date1 = lst[count]
+        date2 = lst[count + 1]
+        print(add_months(date1, 6))
+        if add_months(date1, 6) != date2:
+            new_list = lst2 + [(date2 - date1)]
+            print('not equal')
+            return make_span_list((count + 1), lst, new_list)
+        elif add_months(date1, 6) == date2:
+            new_list = lst2
+            print('equal')
+            return make_span_list((count + 1), lst, new_list)
+    else:
+        return lst2
+
+
 def vs_term():
-    print_all(
-        make_date_list(
-            make_term_list(
-                get_terms(0, get_input('How many terms total?\n'), []))))
+    date_list = make_date_list(
+        make_term_list(
+            get_terms(0, get_input('How many terms total?\n'), [])))
+    print_all(make_span_list(0, date_list, []))
 
 
 if __name__ == "__main__":
